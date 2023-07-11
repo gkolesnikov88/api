46463e1f09ea2df95f553e1ecd8bd198dc583733
George Kolesnikov <gkolesnikov88@gmail.com>
2023-07-11 07:37:11 +0300

    Add to git

--- src/schema.js
+++ src/schema.js
@@ -2,17 +2,22 @@ const { gql } = require('apollo-server-express');
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  scalar DateTime,
   type Note {
     id: ID!
     content: String!
-    author: String!
+    author: String!,
+    createdAt: DateTime!,
+    updatedAt: DateTime!
   }
   type Query {
     notes: [Note!]!
     note(id: ID!): Note!
   }
   type Mutation {
-    newNote(content: String!): Note!
+    newNote(content: String!): Note!,
+    updateNote(id: ID!, content: String!): Note!,
+    deleteNote(id: ID!): Boolean!
   }
 `;
 
