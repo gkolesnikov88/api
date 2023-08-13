2ca1ad0b80a26221d5134edda68681540388f7a1
George Kolesnikov <gkolesnikov88@gmail.com>
2023-08-13 20:39:59 +0300

    Retrieve user, users and current user

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -4,5 +4,17 @@ module.exports = {
   },
   note: async (_parent, args, { models }) => {
     return await models.Note.findById(args.id);
+  },
+  user: async (_parent, { username }, { models }) => {
+    // find a user given their username
+    return await models.User.findOne({ username });
+  },
+  users: async (_parent, _args, { models }) => {
+    // find all users
+    return await models.User.find({});
+  },
+  me: async (_parent, args, { models, user }) => {
+    // find a user given the current user context
+    return await models.User.findById(user.id);
   }
 };
--- src/schema.js
+++ src/schema.js
@@ -13,6 +13,9 @@ const typeDefs = gql`
   type Query {
     notes: [Note!]!
     note(id: ID!): Note!
+    user(username: String!): User
+    users: [User!]!
+    me: User!
   }
   type Mutation {
     newNote(content: String!): Note!,
