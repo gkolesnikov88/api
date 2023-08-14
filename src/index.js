831022a9e6b867c5b8167fc5e536579893f7c1ff
George Kolesnikov <gkolesnikov88@gmail.com>
2023-08-14 08:28:49 +0300

    Add helmet and cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const cors = require('cors');
 require('dotenv').config();
 
 // Local module imports
@@ -15,6 +17,8 @@ const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
 
 const app = express();
+app.use(helmet());
+app.use(cors());
 
 // Connect to the database
 db.connect(DB_HOST);
